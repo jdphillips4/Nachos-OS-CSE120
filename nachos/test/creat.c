56ab0c1119212350d15bc935d55947f9385ba250
sarenaa <122493371+sarenaa@users.noreply.github.com>
2024-11-08 12:30:40 -0800

    Create creat.c

---
+++ nachos/test/creat.c
@@ -0,0 +1,35 @@
+#include "syscall.h"
+//coff is already made when make
+int main (int argc, char *argv[])
+{
+   char *filename1 = "testfile1.txt"; //create new file
+   char *filename2 = "testfile2.txt"; //open existing file
+   char *filename3 = "notexist.txt"; //doesnt exist
+   int fd1, fd2, fd3;
+
+   //test 1: create new file
+    fd1 = creat(filename1);
+    if(fd == -1) write(1, "Failed to create testfile1.txt");
+    else{
+        write( 1, "Successfully created testfile1.txt");
+        close(fd1);
+    }
+
+    //test 2: open existing file
+    fd2 = open(filename1);
+    if(fd2==-1) write(1, "Failed to open testfile1.txt");
+    else{
+        write(1, "Successfully opened testfile1.txt");
+        close(fd2);
+    }
+
+    //test 3:
+    fd3 = open(filename3);
+    if(fd3==-1) write(1, "Failed to open notexist.txt");
+    else{
+        write(1, "this shouldnt run or u have an issue");
+        close(fd3);
+    }
+
+    return 0;
+}
