570853bd59cc7d4c6d35dc2f133a1c8de468da57
sarenaa <122493371+sarenaa@users.noreply.github.com>
2024-11-25 16:01:15 -0800

    Update VMProcess.java

--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -39,7 +39,72 @@ public class VMProcess extends UserProcess {
 	 * @return <tt>true</tt> if successful.
 	 */
 	protected boolean loadSections() {
-		return super.loadSections();
+		pageTable = new TranslationEntry[numPages]; 
+		if( numPages > UserKernel.freePages.size() ){
+			coff.close();
+			Lib.debug(dbgProcess, "\tinsufficient physical memory"); 
+			return false;
+		}
+		//for loop thru page table not coff sections
+		for (int i = 0; i < numPages; i++){
+			int freePage = UserKernel.freePages.pop();
+	 		pageTable[i] = new TranslationEntry(i, freePage, false, false, false, false);
+			//preallocate pages and set valid bit to false
+			//go to handle exception
+		}
+		
+
+		//if( demanded page found ) return super.loadSections();
+		//starting case. check no pages in memory. page fault to laod 1st page into memory
+		//if(freepagelist not empty) return super.loadSections()
+		UserKernel.pageLock.acquire(); 
+		// load sections.keep same?
+		for (int s = 0; s < coff.getNumSections(); s++) {
+			CoffSection section = coff.getSection(s);
+			Lib.debug(dbgProcess, "\tinitializing " + section.getName()
+					+ " section (" + section.getLength() + " pages)");
+			boolean isReadOnly = section.isReadOnly();
+
+			for (int i = 0; i < section.getLength(); i++) {
+				int vpn = section.getFirstVPN() + i;
+				int availPage = UserKernel.freePages.pollLast();
+				pageTable[vpn] = new TranslationEntry(vpn, availPage , false, isReadOnly, false, false);//
+				section.loadPage( i, availPage ); //loads into physical memory
+			}
+		}
+		// allocate stack and argument pages. 0 filled for part 1. ppn=-1 part 2?
+		for (int i = numPages - 1; i >= numPages - 9; i--) {
+			//load demanded page into available free page frame
+			int freePage = UserKernel.freePages.pop();
+			pageTable[i] = new TranslationEntry(i, freePage , true, false, false, false);
+		}
+
+		//ELSE no free page frame, evict a victim page 
+		//page replacement clockwise algorithm (here or handleException?)
+			//use for loop
+			//if(used==true) used=false //2nd chance to pg in case it being used
+			//else evict this victim page, store location next to evicted pg to start next iteration here
+		//if( victimPage is dirty ) PART 2 swap out; WRITE to swap file. 
+			//unset validbit? entry vpn ppn useless. use ppn to store swap pg number in swap file
+			//aka vpn to spn mapping in pgtable
+			//notify owner process (current process requesting page) when dirty pg evicted
+			//update pg table entry of op to mark victim page invalid. usedbit=false
+			//access page table of op by ipt? do we make ipt in kernel or process?
+		File.write( 2*pageSize, memory, paddr, pageSize); //add it to swapFileList
+			//PART 3 optimized to only when page u wanna evict is dirty
+		//else dirty=false, orig pg in coff already so super.loadSections()?
+		// SWAP IN IF PG TO BE ACCESSED ALREADY RESIDES IN SWAP FILE (SWAPPED OUT EARLIER).
+		File.read( 2*pageSize, memory, paddr, pageSize);
+		//swap using StubFileSystem.java calls
+		ThreadedKernel.fileSystem.open();
+		//read/write
+		//close
+		ThreadedKernel.fileSystem.remove();
+		//close/remove swap file before terminate kernel. call vmkernel or userkernel
+
+		//invalidate pge table entry of victim
+		UserKernel.pageLock.release(); 
+		return true;
 	}
 
 	/**
@@ -53,13 +118,38 @@ public class VMProcess extends UserProcess {
 	 * Handle a user exception. Called by <tt>UserKernel.exceptionHandler()</tt>
 	 * . The <i>cause</i> argument identifies which exception occurred; see the
 	 * <tt>Processor.exceptionZZZ</tt> constants.
-	 * 
+	 * try to use page but its invalid. 
+	 * load pages in here on demand ONLY FOR PG EXCEPTIONS. HANDLE NORMAL CASE in loadsections
 	 * @param cause the user exception that occurred.
 	 */
 	public void handleException(int cause) {
+		boolean isCoff = false;
 		Processor processor = Machine.processor();
-
 		switch (cause) {
+			case Processor.exceptionPageFault:
+				UserKernel.pageLock.acquire(); 
+				//load single pg that caused exception
+				int virtualAddr = processor.readRegister(Processor.regBadVAddr);//doesnt match to phys pg#
+				//convert to vpn
+				int vpn = virtualAddr / pageSize;
+				//tell difference btwn coff page and stack argument pg. 
+				//loop thru all coff section, see if vpn is in that coff section. if so load from that coff section.
+				for (int s = 0; s < coff.getNumSections(); s++) {
+					CoffSection section = coff.getSection(s);
+					for (int i = 0; i < section.getLength(); i++) {
+
+						if( vpn == section.getFirstVPN() + i ){//coff
+							section.loadPage( i, pageTable[vpn].ppn );
+							pageTable[vpn].valid = true;
+							isCoff = true;
+						}
+					}
+				}
+				if( isCoff == false ){ //0fill that physical page
+					pageTable[ppn].ze
+					//zero out pg size elements in that array
+				}
+										
 		default:
 			super.handleException(cause);
 			break;
@@ -71,4 +161,7 @@ public class VMProcess extends UserProcess {
 	private static final char dbgProcess = 'a';
 
 	private static final char dbgVM = 'v';
+
+	//changes here
+	// protected TranslationEntry[] invertedPageTable;
 }
